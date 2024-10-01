907c338b8e5d49ea7945252f15f2ed416641e607
MilanVW1 <r0985432@student.thomasmore.be>
2024-10-01 11:44:23 +0200

    verder gewerkt aan opdracht astro.. of toch geprobeerd verder te werken...

---
+++ frontend/src/pages/script.js
@@ -0,0 +1,20 @@
+
+// let main = document.querySelector("#mainDiv") 
+
+// let slider = document.querySelector("#discount-slider") // Ophalen van de slider zelf
+// let sliderTekst = document.querySelector("#sliderValue") 
+// let sliderValue = 0; // Value van de slider initialiseren
+
+// let searchButton = document.querySelector("#searchButton") 
+// let searchInput = document.querySelector("#simple-search") 
+
+// slider?.addEventListener("input", (event) => {
+//     let inputValue = event.target
+//     sliderValue = parseInt(inputValue.value) // sliderValue instellen op het inputValue
+//     console.log(sliderValue)
+    
+//     sliderTekst.textContent = "Current value: " + sliderValue + "%" // <p> element opvullen met de value van de slider.	
+//     const filtered = json.filter(g => g.discountPercentage != null && g.discountPercentage >= sliderValue)
+
+//     console.log(filtered)
+// }) // event toevoegen aan de slider, die elke 'slide' bijhoudt.
