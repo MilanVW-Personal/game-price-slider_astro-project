648ccf25072f60704630ffac9575e8e7e6dd1341
MilanVW1 <r0985432@student.thomasmore.be>
2024-10-06 21:14:17 +0200

    download van artifact aangepast

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -73,7 +73,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run start',
-    url: 'http://localhost:4321/',
+    url: 'http://localhost:4321/astro-build',
     reuseExistingServer: !process.env.CI,
   },
 });
